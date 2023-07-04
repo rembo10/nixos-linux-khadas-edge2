6bdbbfa857b14572ce986917e9f2af3d425bf0a5
rembo10 <rembo10@users.noreply.github.com>
2023-07-04 11:22:46 +0530

    Add armbian to master

--- flake.nix
+++ flake.nix
@@ -1,10 +1,17 @@
 {
 
   inputs = {
+
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+
+    rk3588-implicit-sync = {
+      url = gitlab:panfork/rk3588-implicit-sync;
+      flake = false;
+    };
+
   };
 
-  outputs = { self, nixpkgs }: 
+  outputs = inputs@{ self, nixpkgs, ... }: 
     let
       pkgs = import nixpkgs {
         system = "x86_64-linux";
@@ -20,13 +27,35 @@
           })
         ];
       };
-      linux_khadas_edges = (pkgs.linuxManualConfig {
+
+      linux_armbian = (pkgs.linuxManualConfig {
+        version = "5.10.160";
+        src = pkgs.fetchzip {
+          url = "https://github.com/armbian/linux-rockchip/archive/99a6d693684e987cd843d6e9bf71ebcb55de51f0.zip";
+          hash = "sha256-Ncw+x84AwScG/aZtMndmDueBEopHtqO4+oqYgfWNd7U=";
+        };
+        configfile = ./config_armbian;
+        kernelPatches = [
+        ];
+        allowImportFromDerivation = true;
+      })
+      .overrideAttrs (old: {
+        name = "k";
+        nativeBuildInputs = old.nativeBuildInputs ++ (with pkgs.buildPackages; [
+          ubootTools
+        ]);
+#        prePatch = ''
+#          rm arch/arm64/boot/dts/rockchip/*.dts* !("rk3588s-khadas-edge2.dts"|"rk3588s.dtsi"|"rk3588s-khadas-edge2.dtsi"|"rk3588-rk806-single-khadas.dtsi"|"rk3588-linux.dtsi"|"rk3588s-khadas-edge2-camera.dtsi")
+ #       '';
+      });
+
+      linux_khadas = (pkgs.linuxManualConfig {
         version = "5.10.66";
         src = pkgs.fetchzip {
           url = "https://github.com/khadas/linux/archive/refs/tags/khadas-edges-linux-5.10-v1.5-release.tar.gz";
           hash = "sha256-7SDRxUHsM8xM4Dgtd6VxBQYpnVKCe7RH9BB1JDVWx7U=";
         };
-        configfile = ./config;
+        configfile = ./config_khadas;
         kernelPatches = [
           { name = "bcmdhd-sourcetree-fix"; patch = ./patches/bcmdhd-sourcetree-fix.patch; }
         ];
@@ -34,12 +63,24 @@
       })
       .overrideAttrs (old: {
         name = "k";
-        nativeBuildInputs = old.nativeBuildInputs ++ [pkgs.buildPackages.ubootTools];
+        nativeBuildInputs = old.nativeBuildInputs ++ (with pkgs.buildPackages; [
+          ubootTools
+        ]);
       });
+
     in {
-      pkgsCross.aarch64-multiplatform.linux_khadas_edges = linux_khadas_edges;
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        inputsFrom = [ self.pkgsCross.aarch64-multiplatform.linux_khadas_edges ];
+
+      packages.x86_64-linux = {
+        linux_khadas = linux_khadas;
+        linux_armbian = linux_armbian;
       };
+
+      devShells.x86_64-linux.default = linux_khadas.overrideAttrs (old: {
+        nativeBuildInputs = old.nativeBuildInputs ++ (with pkgs; [ ncurses pkg-config ]);
+        shellHook = ''
+          addToSearchPath PKG_CONFIG_PATH ${pkgs.buildPackages.ncurses.dev}/lib/pkgconfig
+        '';
+        });
+
     };
 }
